16256c9db1f49b995b1d7aa67eceae1ef17c3923
Scruffy pc <cayabyabrr@yahoo.com>
2023-02-19 22:22:43 -0800

    comment out not used

--- src/components/DraggableComp.js
+++ src/components/DraggableComp.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState, useRef } from "react"
+// import React, { useEffect, useState, useRef } from "react"
 
-export function DraggableComp () {
+// export function DraggableComp () {
 
-const [pressed, setPressed] = useState(false)
-const [position, setPosition] = useState({x: 0, y: 0})
-const ref = useRef()
+// const [pressed, setPressed] = useState(false)
+// const [position, setPosition] = useState({x: 0, y: 0})
+// const ref = useRef()
 
-useEffect(() => {
-if (ref.current) {
-    ref.current.style.transform= `translate(${position.x}px, ${position.y}.px)`
-}
-}, [position])
+// useEffect(() => {
+// if (ref.current) {
+//     ref.current.style.transform= `translate(${position.x}px, ${position.y}.px)`
+// }
+// }, [position])
 
-const onMouseMove = (e) => {
-if (pressed) {
-    setPosition({
-        x: position.x + e.movementX,
-        y: position.y + e.movementY
-    })
-}
-}
+// const onMouseMove = (e) => {
+// if (pressed) {
+//     setPosition({
+//         x: position.x + e.movementX,
+//         y: position.y + e.movementY
+//     })
+// }
+// }
 
-return (
-    <>
-    <div className="absolute bg-slate-200 w-full h-full"
-                    onMouseMove={onMouseMove}
-                    onMouseDown={() => setPressed(true)}
-                    onMouseUp={() => setPressed(false)}
-    >
-                <div 
-                                    // onMouseMove={onMouseMove}
-                                    // onMouseDown={() => setPressed(true)}
-                                    // onMouseUp={() => setPressed(false)}
-                                    ref={ref}
-                className="
-                w-[200px]
-                h-[200px]
-                bg-yellow-300
-                flex resize overflow-hidden
-                justify-center
-                "
-            >
-                <p className=" select-none">{pressed ? "dragging..." : "press to drag"}</p>
-            </div>
-            </div>
-            <button onClick={() => setPosition({x: 0, y: 0})} className="absolute bg-blue-400">Reset position</button>
-    </>
-)}
+// return (
+//     <>
+//     <div className="absolute bg-slate-200 w-full h-full"
+//                     onMouseMove={onMouseMove}
+//                     onMouseDown={() => setPressed(true)}
+//                     onMouseUp={() => setPressed(false)}
+//     >
+//                 <div 
+//                                     // onMouseMove={onMouseMove}
+//                                     // onMouseDown={() => setPressed(true)}
+//                                     // onMouseUp={() => setPressed(false)}
+//                                     ref={ref}
+//                 className="
+//                 w-[200px]
+//                 h-[200px]
+//                 bg-yellow-300
+//                 flex resize overflow-hidden
+//                 justify-center
+//                 "
+//             >
+//                 <p className=" select-none">{pressed ? "dragging..." : "press to drag"}</p>
+//             </div>
+//             </div>
+//             <button onClick={() => setPosition({x: 0, y: 0})} className="absolute bg-blue-400">Reset position</button>
+//     </>
+// )}
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import StatsRow from './StatsRow'
 import Draggable from 'react-draggable'
